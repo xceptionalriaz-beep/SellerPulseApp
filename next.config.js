6246a8ce11c18057b543f57bb63cb0d310b53ce4
Reaz Uddin <xceptionalriaz@gmail.com>
2026-06-15 04:24:39 +0600

    Fix Edge runtime - externalize @supabase/ssr from middleware bundle

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@supabase/ssr'],
+  },
   images: {
     remotePatterns: [
       {
